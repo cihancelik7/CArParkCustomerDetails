a7db1c818060d8d5897a1467338369a9db7bc563
Cihan0201 <114297546+Cihan0201@users.noreply.github.com>
2023-10-19 23:30:56 +0300

    bntAdd connected

--- app/src/main/java/com/cihancelik/carparkcustomerdetails/MainActivity.kt
+++ app/src/main/java/com/cihancelik/carparkcustomerdetails/MainActivity.kt
@@ -2,10 +2,97 @@ package com.cihancelik.carparkcustomerdetails
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etName: EditText
+    private lateinit var etLastName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPhone: EditText
+    private lateinit var etAddress: EditText
+    private lateinit var etCity: EditText
+    private lateinit var etCarPlate: EditText
+    private lateinit var btnAdd: Button
+    private lateinit var btnView: Button
+
+    private lateinit var sqLiteHelper: SQLiteHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+
+        sqLiteHelper = SQLiteHelper(this)
+
+
+        btnAdd.setOnClickListener { addCustomer() }
+        btnView.setOnClickListener {  }
+
+    }
+
+    private fun addCustomer() {
+        val name = etName.text.toString()
+        val lastName = etLastName.text.toString()
+        val email = etEmail.text.toString()
+        val phone = etPhone.text.toString()
+        val address = etAddress.text.toString()
+        val city = etCity.text.toString()
+        val carPlate = etCarPlate.text.toString()
+
+        if (name.isEmpty() || lastName.isEmpty() || email.isEmpty() || phone.isEmpty()
+            || address.isEmpty() || city.isEmpty() || carPlate.isEmpty()
+        ) {
+            Toast.makeText(this, "Please Enter Requirement Field", Toast.LENGTH_SHORT).show()
+        } else {
+            val cst = CustomerModel(
+                name = name,
+                lastName = lastName,
+                email = email,
+                phone = phone,
+                address = address,
+                city = city,
+                carplate = carPlate
+            )
+            val status = sqLiteHelper.insertCustomer(cst)
+            // check insert success or not success
+            if (status > -1) {
+                Toast.makeText(this, "Customer Added", Toast.LENGTH_SHORT).show()
+                clearEditText()
+
+            } else {
+                Toast.makeText(this, "Record not saved", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun clearEditText() {
+        etName.setText("")
+        etLastName.setText("")
+        etEmail.setText("")
+        etPhone.setText("")
+        etAddress.setText("")
+        etCity.setText("")
+        etCarPlate.setText("")
+
+        etName.requestFocus()
+
+
+    }
+
+    private fun initView() {
+
+        etName = findViewById(R.id.cstName)
+        etLastName = findViewById(R.id.cstLastName)
+        etEmail = findViewById(R.id.cstEmail)
+        etPhone = findViewById(R.id.cstPhone)
+        etAddress = findViewById(R.id.cstAddress)
+        etCity = findViewById(R.id.cstCity)
+        etCarPlate = findViewById(R.id.cstCarNumberPlate)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnView = findViewById(R.id.btnView)
+
     }
 }
--- app/src/main/java/com/cihancelik/carparkcustomerdetails/SQLiteHelper.kt
+++ app/src/main/java/com/cihancelik/carparkcustomerdetails/SQLiteHelper.kt
@@ -2,6 +2,7 @@ package com.cihancelik.carparkcustomerdetails
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
@@ -36,22 +37,78 @@ class SQLiteHelper(context: Context) :
 
     }
 
-    fun insertCustomer(customer: CustomerModel) :Long{
+    fun insertCustomer(customer: CustomerModel): Long {
 
         val db = this.writableDatabase
         val contentValues = ContentValues()
-        contentValues.put(ID,customer.id)
-        contentValues.put(NAME,customer.name)
-        contentValues.put(LASTNAME,customer.lastName)
-        contentValues.put(EMAIL,customer.email)
-        contentValues.put(PHONE,customer.phone)
-        contentValues.put(ADDRESS,customer.address)
-        contentValues.put(CITY,customer.city)
-        contentValues.put(CARPLATE,customer.carplate)
+        contentValues.put(ID, customer.id)
+        contentValues.put(NAME, customer.name)
+        contentValues.put(LASTNAME, customer.lastName)
+        contentValues.put(EMAIL, customer.email)
+        contentValues.put(PHONE, customer.phone)
+        contentValues.put(ADDRESS, customer.address)
+        contentValues.put(CITY, customer.city)
+        contentValues.put(CARPLATE, customer.carplate)
 
-        val success = db.insert(TABLE_CUSTOMER,null,contentValues)
+        val success = db.insert(TABLE_CUSTOMER, null, contentValues)
         db.close()
         return success
+    }
+
+    fun getAllCustomers(): ArrayList<CustomerModel> {
+        val customerList: ArrayList<CustomerModel> = ArrayList()
+        val selectQuery = "SELECT * FROM $TABLE_CUSTOMER"
+        val db = this.readableDatabase
+
+        val cursor: Cursor?
+
+        try {
+            cursor = db.rawQuery(selectQuery, null)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            db.execSQL(selectQuery)
+            return ArrayList()
+        }
+        var id: Int
+        var name: String
+        var lastName: String
+        var email: String
+        var phone: String
+        var address: String
+        var city: String
+        var carplate: String
+
+
+        if (cursor.moveToFirst()) {
+            do {
+                id = cursor.getInt(cursor.getColumnIndex("id"))
+                name = cursor.getString(cursor.getColumnIndex("name"))
+                lastName = cursor.getString(cursor.getColumnIndex("lastname"))
+                email = cursor.getString(cursor.getColumnIndex("email"))
+                phone = cursor.getString(cursor.getColumnIndex("phone"))
+                address = cursor.getString(cursor.getColumnIndex("address"))
+                city = cursor.getString(cursor.getColumnIndex("city"))
+                carplate = cursor.getString(cursor.getColumnIndex("carplate"))
+                val cst = CustomerModel(
+                    id = id,
+                    name = name,
+                    lastName = lastName,
+                    email = email,
+                    phone = phone,
+                    address = address,
+                    city = city,
+                    carplate = carplate
+                )
+
+                customerList.add(cst)
+
+            } while (cursor.moveToNext())
+        }
+
+        return customerList
+
+
     }
 
 }
