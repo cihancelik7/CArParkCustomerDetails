9e914eef15f89ee5417dce2ba867a93d319e7588
Cihan0201 <114297546+Cihan0201@users.noreply.github.com>
2023-10-26 11:17:59 +0300

    employee main activity already finished

--- app/src/main/java/com/cihancelik/carparkcustomerdetails/CustomerMainActivity.kt
+++ app/src/main/java/com/cihancelik/carparkcustomerdetails/CustomerMainActivity.kt
@@ -59,8 +59,6 @@ class CustomerMainActivity : AppCompatActivity() {
     }
 
 
-
-
     private fun updateCustomer() {
         val name = etName.text.toString()
         val lastName = etLastName.text.toString()
@@ -86,7 +84,7 @@ class CustomerMainActivity : AppCompatActivity() {
             if (status > -1) {
                 Toast.makeText(this, "Update successful", Toast.LENGTH_SHORT).show()
                 cst = updatedCustomer  // Güncellenen verileri "cst" değişkenine atayın
-                val intent = Intent(this,CustomerMainActivity::class.java)
+                val intent = Intent(this, CustomerMainActivity::class.java)
                 startActivity(intent)
             } else {
                 Toast.makeText(this, "Update failed...", Toast.LENGTH_SHORT).show()
@@ -95,68 +93,69 @@ class CustomerMainActivity : AppCompatActivity() {
     }
 
 
-private fun addCustomer() {
-    val name = etName.text.toString()
-    val lastName = etLastName.text.toString()
-    val email = etEmail.text.toString()
-    val phone = etPhone.text.toString()
-    val address = etAddress.text.toString()
-    val city = etCity.text.toString()
-    val carPlate = etCarPlate.text.toString()
+    private fun addCustomer() {
+        val name = etName.text.toString()
+        val lastName = etLastName.text.toString()
+        val email = etEmail.text.toString()
+        val phone = etPhone.text.toString()
+        val address = etAddress.text.toString()
+        val city = etCity.text.toString()
+        val carPlate = etCarPlate.text.toString()
 
-    if (name.isEmpty() || lastName.isEmpty() || email.isEmpty() || phone.isEmpty()
-        || address.isEmpty() || city.isEmpty() || carPlate.isEmpty()
-    ) {
-        Toast.makeText(this, "Please Enter Requirement Field", Toast.LENGTH_SHORT).show()
-    } else {
-        val cst = CustomerModel(
-            name = name,
-            lastName = lastName,
-            email = email,
-            phone = phone,
-            address = address,
-            city = city,
-            carplate = carPlate
-        )
-        val status = sqLiteHelper.insertCustomer(cst)
-        // check insert success or not success
-        if (status > -1) {
-            Toast.makeText(this, "Customer Added", Toast.LENGTH_SHORT).show()
-            clearEditText()
-
-
+        if (name.isEmpty() || lastName.isEmpty() || email.isEmpty() || phone.isEmpty()
+            || address.isEmpty() || city.isEmpty() || carPlate.isEmpty()
+        ) {
+            Toast.makeText(this, "Please Enter Requirement Field", Toast.LENGTH_SHORT).show()
         } else {
-            Toast.makeText(this, "Record not saved", Toast.LENGTH_SHORT).show()
+            val cst = CustomerModel(
+                name = name,
+                lastName = lastName,
+                email = email,
+                phone = phone,
+                address = address,
+                city = city,
+                carplate = carPlate
+            )
+            val status = sqLiteHelper.insertCustomer(cst)
+            // check insert success or not success
+            if (status > -1) {
+                Toast.makeText(this, "Customer Added", Toast.LENGTH_SHORT).show()
+                clearEditText()
+
+
+            } else {
+                Toast.makeText(this, "Record not saved", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
 
+    private fun clearEditText() {
+        etName.setText("")
+        etLastName.setText("")
+        etEmail.setText("")
+        etPhone.setText("")
+        etAddress.setText("")
+        etCity.setText("")
+        etCarPlate.setText("")
+
+        etName.requestFocus()
+
+
+    }
+
+
+    private fun initView() {
+
+        etName = findViewById(R.id.cstName)
+        etLastName = findViewById(R.id.cstLastName)
+        etEmail = findViewById(R.id.cstEmail)
+        etPhone = findViewById(R.id.cstPhone)
+        etAddress = findViewById(R.id.cstAddress)
+        etCity = findViewById(R.id.cstCity)
+        etCarPlate = findViewById(R.id.cstCarNumberPlate)
+        btnAdd = findViewById(R.id.btnCstAdd)
+        btnView = findViewById(R.id.btnCstView)
+        btnUpdate = findViewById(R.id.btnCstUpdate)
+    }
 }
-
-private fun clearEditText() {
-    etName.setText("")
-    etLastName.setText("")
-    etEmail.setText("")
-    etPhone.setText("")
-    etAddress.setText("")
-    etCity.setText("")
-    etCarPlate.setText("")
-
-    etName.requestFocus()
-
-
-}
-
-
-private fun initView() {
-
-    etName = findViewById(R.id.cstName)
-    etLastName = findViewById(R.id.cstLastName)
-    etEmail = findViewById(R.id.cstEmail)
-    etPhone = findViewById(R.id.cstPhone)
-    etAddress = findViewById(R.id.cstAddress)
-    etCity = findViewById(R.id.cstCity)
-    etCarPlate = findViewById(R.id.cstCarNumberPlate)
-    btnAdd = findViewById(R.id.btnAdd)
-    btnView = findViewById(R.id.btnView)
-    btnUpdate = findViewById(R.id.btnUpdate)
-}}
