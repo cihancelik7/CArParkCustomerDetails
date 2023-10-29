607e194f24508248ebce86cad30679addc21a987
Cihan0201 <114297546+Cihan0201@users.noreply.github.com>
2023-10-29 15:00:36 +0300

    Ui creation started

---
+++ app/src/main/java/com/cihancelik/CarParkDetails/HR/HrViewPage.kt
@@ -0,0 +1,12 @@
+package com.cihancelik.CarParkDetails.HR
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cihancelik.carparkcustomerdetails.R
+
+class HrViewPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hr_view_page)
+    }
+}
