a27e38be7be87c8ddc2e2a3ccc6ea789102cce3e
Cihan0201 <114297546+Cihan0201@users.noreply.github.com>
2023-12-01 19:38:37 +0300

    Hr Org Main Screen xml file finished

--- app/src/main/java/com/cihancelik/CarParkDetails/HR/hrOrganizations/HrOrganizationsAdapter.kt
+++ app/src/main/java/com/cihancelik/CarParkDetails/HR/hrOrganizations/HrOrganizationsAdapter.kt
@@ -1,4 +1,31 @@
 package com.cihancelik.CarParkDetails.HR.hrOrganizations
 
-class HrOrganizationsAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.cihancelik.carparkcustomerdetails.R
+
+class HrOrganizationsAdapter:RecyclerView.Adapter<HrOrganizationsAdapter.HrOrganizationViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HrOrganizationsAdapter.HrOrganizationViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        holder: HrOrganizationsAdapter.HrOrganizationViewHolder,
+        position: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+class HrOrganizationViewHolder(var view: View):RecyclerView.ViewHolder(view){
+
+
+}
 }
