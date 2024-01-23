074487cb5fb4f9f67158d42722b3c373c1a03a95
Cihan0201 <114297546+Cihan0201@users.noreply.github.com>
2024-01-24 02:30:42 +0300

    hr salaries started.

--- app/src/main/java/com/cihancelik/CarParkDetails/HR/hrSalaries/HrSalariesMainActivity.kt
+++ app/src/main/java/com/cihancelik/CarParkDetails/HR/hrSalaries/HrSalariesMainActivity.kt
@@ -1,5 +1,6 @@
 package com.cihancelik.CarParkDetails.HR.hrSalaries
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,20 +11,20 @@ import com.cihancelik.CarParkDetails.SQL.hr.SQLiteHelperForHrSalaries
 import com.cihancelik.carparkcustomerdetails.R
 
 class HrSalariesMainActivity : AppCompatActivity() {
-    private lateinit var etEmpId : EditText
-    private lateinit var etAmount : EditText
-    private lateinit var etStartDate : EditText
-    private lateinit var etEndDate : EditText
-    private lateinit var etUpdateDate : EditText
-    private lateinit var etCreationDate :EditText
+    private lateinit var etEmpId: EditText
+    private lateinit var etAmount: EditText
+    private lateinit var etStartDate: EditText
+    private lateinit var etEndDate: EditText
+    private lateinit var etUpdateDate: EditText
+    private lateinit var etCreationDate: EditText
 
     private lateinit var btnAdd: Button
-    private lateinit var btnView : Button
-    private lateinit var btnUpdate : Button
+    private lateinit var btnView: Button
+    private lateinit var btnUpdate: Button
 
-    private lateinit var sqliteHelperForHrSalaries:SQLiteHelperForHrSalaries
+    private lateinit var sqliteHelperForHrSalaries: SQLiteHelperForHrSalaries
     private lateinit var sqLiteHelperForHrEmployees: SQLiteHelperForHrEmployees
-    private var hrSalaryInfo1 : HrSalariesModel? = null
+    private var hrSalaryInfo1: HrSalariesModel? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hr_salaries_main)
@@ -31,9 +32,10 @@ class HrSalariesMainActivity : AppCompatActivity() {
         sqliteHelperForHrSalaries = SQLiteHelperForHrSalaries(this)
         sqLiteHelperForHrEmployees = SQLiteHelperForHrEmployees(this)
 
-        val selectedHrsalaryInfo = intent.getSerializableExtra("selectedHrSalaryInfo") as? HrSalariesModel
+        val selectedHrsalaryInfo =
+            intent.getSerializableExtra("selectedHrSalaryInfo") as? HrSalariesModel
 
-        if (selectedHrsalaryInfo!=null){
+        if (selectedHrsalaryInfo != null) {
             etEmpId.setText(selectedHrsalaryInfo.employeeId.toString())
             etAmount.setText(selectedHrsalaryInfo.amount.toString())
             etStartDate.setText(selectedHrsalaryInfo.startDate)
@@ -44,6 +46,62 @@ class HrSalariesMainActivity : AppCompatActivity() {
             hrSalaryInfo1 = selectedHrsalaryInfo
         }
         btnAdd.setOnClickListener { addHrSalary() }
+        var goToHrSalaryViewActivity = Intent(this, HrSalariesViewActivity::class.java)
+        btnView.setOnClickListener { startActivity(goToHrSalaryViewActivity) }
+
+        var selectedHrSalaryUpdate =
+            intent.getSerializableExtra("selectedHrSalaryUpdate") as? HrSalariesModel
+
+        if (selectedHrSalaryUpdate != null) {
+            etEmpId.setText(selectedHrSalaryUpdate.employeeId.toString())
+            etAmount.setText(selectedHrSalaryUpdate.amount.toString())
+            etStartDate.setText(selectedHrSalaryUpdate.startDate)
+            etEndDate.setText(selectedHrSalaryUpdate.endDate)
+            etUpdateDate.setText(selectedHrSalaryUpdate.updateDate)
+            etCreationDate.setText(selectedHrSalaryUpdate.creationDate)
+            hrSalaryInfo1 = selectedHrsalaryInfo
+        }
+        btnUpdate.setOnClickListener { updateHrSalary() }
+    }
+
+    private fun isUpdate(updatedHrSalary: HrSalariesModel): Boolean {
+        val currentHrSalary = sqliteHelperForHrSalaries.getHrSalaryById(updatedHrSalary.salaryId)
+        return currentHrSalary != updatedHrSalary
+
+    }
+
+    private fun updateHrSalary() {
+        val empId = etEmpId.text.toString()
+        val amount = etAmount.text.toString()
+        val startDate = etStartDate.text.toString()
+        val endDate = etEndDate.text.toString()
+        val updateDate = etUpdateDate.text.toString()
+        val creationDate = etCreationDate.text.toString()
+
+        if (hrSalaryInfo1 != null) {
+            val updatedHrSalary = HrSalariesModel(
+                salaryId = hrSalaryInfo1!!.salaryId,
+                employeeId = hrSalaryInfo1!!.employeeId,
+                amount = amount.toInt(),
+                startDate = startDate,
+                endDate = endDate,
+                updateDate = updateDate,
+                creationDate = creationDate
+            )
+            val isUpdate = isUpdate(updatedHrSalary)
+            if (isUpdate){
+                val status = sqliteHelperForHrSalaries.updateHrSalary(updatedHrSalary)
+                if (status > -1){
+                    Toast.makeText(this, "Update Successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,HrSalariesViewActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this, "Update Failed...", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "No Changes Were Made", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun addHrSalary() {
@@ -54,13 +112,13 @@ class HrSalariesMainActivity : AppCompatActivity() {
         val updateDate = etUpdateDate.text.toString()
         val creationDate = etCreationDate.text.toString()
 
-        if (empIdText.isEmpty()||amount.isEmpty()||startDate.isEmpty()||updateDate.isEmpty()||creationDate.isEmpty()){
+        if (empIdText.isEmpty() || amount.isEmpty() || startDate.isEmpty() || updateDate.isEmpty() || creationDate.isEmpty()) {
             Toast.makeText(this, "Please Enter Requirement Field", Toast.LENGTH_SHORT).show()
-        }else{
+        } else {
             val enteredEmpId = empIdText.toInt()
             val empId = sqLiteHelperForHrEmployees.getHrEmpById(enteredEmpId)
 
-            if (empId!=null){
+            if (empId != null) {
                 val hrSalaryInfo = HrSalariesModel(
                     salaryId = 0,
                     employeeId = enteredEmpId,
@@ -70,11 +128,19 @@ class HrSalariesMainActivity : AppCompatActivity() {
                     updateDate = updateDate,
                     creationDate = creationDate
                 )
-                val status : Long = sqliteHelperForHrSalaries.insertHrSalaries(hrSalaryInfo)
-                if (status>-1){
+                val status: Long = sqliteHelperForHrSalaries.insertHrSalaries(hrSalaryInfo)
+                if (status > -1) {
                     Toast.makeText(this, "Hr Salary Added", Toast.LENGTH_SHORT).show()
                     clearEditText()
+                } else {
+                    Toast.makeText(this, "Record not saved!!!", Toast.LENGTH_SHORT).show()
                 }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Hr Salary With the given employee id or employee id",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
